0c70f0a3ddaf1671024c58634b4e25cd6a9e7717
Brendan Molloy <brendan@technocreatives.com>
2018-02-22 14:57:08 +0100

    Fix window size and progress indication

--- Pahkat/UI/Main/Install/InstallView.swift
+++ Pahkat/UI/Main/Install/InstallView.swift
@@ -11,12 +11,16 @@ import RxSwift
 import RxCocoa
 
 class InstallView: View {
-    
+    @IBOutlet weak var spinningIndicator: NSProgressIndicator!
     @IBOutlet weak var horizontalIndicator: NSProgressIndicator!
     @IBOutlet weak var nameLabel: NSTextField!
     @IBOutlet weak var remainingLabel: NSTextField!
     
     override func awakeFromNib() {
+        horizontalIndicator.controlTint = .blueControlTint
         
+        spinningIndicator.isIndeterminate = true
+        spinningIndicator.usesThreadedAnimation = true
+        spinningIndicator.startAnimation(nil)
     }
 }
