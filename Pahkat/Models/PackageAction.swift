48c8cfece36e427b2fcc5ad4a94d5154b82cc95c
Dylan Hand <dylan.hand@gmail.com>
2020-04-29 10:33:26 +0200

    Theoretically create PackageAction from protobuf

--- Pahkat/Models/PackageAction.swift
+++ Pahkat/Models/PackageAction.swift
@@ -12,4 +12,13 @@ struct PackageAction: Equatable, Hashable {
     let key: PackageKey
     let action: PackageActionType
     let target: SystemTarget
+
+    static func from(_ action: Pahkat_PackageAction) -> Self {
+        let packageKey = PackageKey(repositoryURL: URL(string: "TODO ???")!, id: action.id) // TODO: where to get URL?
+        let actionType = PackageActionType(rawValue: UInt8(action.action))
+        let target = SystemTarget(rawValue: UInt8(action.target))
+        return PackageAction(key: packageKey,
+                             action: actionType ?? PackageActionType.install,
+                             target: target ?? SystemTarget.user)
+    }
 }
