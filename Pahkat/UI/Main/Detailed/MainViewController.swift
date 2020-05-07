719836968aee7c8ae717259ff2674f7d2c92389f
Dylan Hand <dylan.hand@gmail.com>
2020-05-07 09:47:32 +0200

    Fix settings button on main view

--- Pahkat/UI/Main/Detailed/MainViewController.swift
+++ Pahkat/UI/Main/Detailed/MainViewController.swift
@@ -201,7 +201,6 @@ class MainViewController: DisposableViewController<MainView>, MainViewable, NSTo
         
         contentView.outlineView.columnAutoresizingStyle = .lastColumnOnlyAutoresizingStyle
         
-        contentView.settingsButton.isEnabled = false
         self.update(title: Strings.appName)
         updatePrimaryButton(isEnabled: false, label: Strings.noPackagesSelected)
         
