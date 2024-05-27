3dd76c4685812a4fe2bc4c337771d5b4b265fa2d
Dylan Hand <dylan.hand@gmail.com>
2024-05-27 14:07:54 +0200

    Remove former notarize command from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -36,7 +36,6 @@ echo "Notarizing bundle"
 APP_ZIP="Divvun Manager.zip"
 /usr/bin/ditto -c -k --keepParent "$APP_NAME" "$APP_ZIP" 
 xcrun notarytool submit "$APP_ZIP" --apple-id "$INPUT_MACOS_DEVELOPER_ACCOUNT" --password "$INPUT_MACOS_NOTARIZATION_APP_PWD" --team-id "$MACOS_DEVELOPMENT_TEAM" --wait
-#xcnotary notarize "$APP_NAME" --override-path-type app -d "$INPUT_MACOS_DEVELOPER_ACCOUNT" -p "$INPUT_MACOS_NOTARIZATION_APP_PWD"
 xcrun stapler staple "$APP_NAME"
 
 echo "::endgroup::"
